1a6fd4de4007615cece566bb55e546f5773e86b6
Anjaney <asinghbits5@gmail.com>
2020-01-26 22:03:13 +0400

    Testing

---
+++ Days of Code/Testing.java
@@ -0,0 +1,101 @@
+import java.util.*;
+
+public class Testing {
+
+    public static int minimum_index(int[] seq) {
+        if (seq.length == 0) {
+            throw new IllegalArgumentException("Cannot get the minimum value index from an empty sequence");
+        }
+        int min_idx = 0;
+        for (int i = 1; i < seq.length; ++i) {
+            if (seq[i] < seq[min_idx]) {
+                min_idx = i;
+            }
+        }
+        return min_idx;
+    }
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("5");
+        
+        System.out.println("4 4");
+        System.out.println("-1 0 3 3");
+        
+        System.out.println("6 2");
+        System.out.println("-1 0 1 2 3 2");
+        
+        System.out.println("7 4");
+        System.out.println("-1 -2 0 3 3 9 6");
+        
+        System.out.println("3 2");
+        System.out.println("-2 0 1");
+        
+        System.out.println("8 5");
+        System.out.println("-2 -1 0 2 3 5 6 99");
+
+    }
+}
+    
+ public static void TestWithEmptyArray() {
+        try {
+            int[] seq = TestDataEmptyArray.get_array();
+            int result = minimum_index(seq);
+        } catch (IllegalArgumentException e) {
+            return;
+        }
+        throw new AssertionError("Exception wasn't thrown as expected");
+    }
+
+    public static void TestWithUniqueValues() {
+        int[] seq = TestDataUniqueValues.get_array();
+        if (seq.length < 2) {
+            throw new AssertionError("less than 2 elements in the array");
+        }
+
+        Integer[] tmp = new Integer[seq.length];
+        for (int i = 0; i < seq.length; ++i) {
+            tmp[i] = Integer.valueOf(seq[i]);
+        }
+        if (!((new LinkedHashSet<Integer>(Arrays.asList(tmp))).size() == seq.length)) {
+            throw new AssertionError("not all values are unique");
+        }
+
+        int expected_result = TestDataUniqueValues.get_expected_result();
+        int result = minimum_index(seq);
+        if (result != expected_result) {
+            throw new AssertionError("result is different than the expected result");
+        }
+    }
+
+    public static void TestWithExactlyTwoDifferentMinimums() {
+        int[] seq = TestDataExactlyTwoDifferentMinimums.get_array();
+        if (seq.length < 2) {
+            throw new AssertionError("less than 2 elements in the array");
+        }
+
+        int[] tmp = seq.clone();
+        Arrays.sort(tmp);
+        if (!(tmp[0] == tmp[1] && (tmp.length == 2 || tmp[1] < tmp[2]))) {
+            throw new AssertionError("there are not exactly two minimums in the array");
+        }
+
+        int expected_result = TestDataExactlyTwoDifferentMinimums.get_expected_result();
+        int result = minimum_index(seq);
+        if (result != expected_result) {
+            throw new AssertionError("result is different than the expected result");
+        }
+    }
+
+    public static void main(String[] args) {
+        TestWithEmptyArray();
+        TestWithUniqueValues();
+        TestWithExactlyTwoDifferentMinimums();
+        System.out.println("OK");
+    }
+}
+30 days of code solution day 27 hackerrank
